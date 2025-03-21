293b34ba24aee71c10330ad6accb940cb74761dc
Simon Luckhoff <simonluckhoff@gmail.com>
2025-03-21 12:51:12 +0200

    phpcalc

---
+++ functions.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>calculation</title>
+  <link rel="icon" href="/Calculator/calc.png" type="image/png">
+  <style>
+    body {
+      background-color: #478589;
+      color: #f1f1f1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding-top: 100px;
+    }
+
+    h1{
+      font-size: 4rem;
+      color: #f1f1f1;
+      margin: 0;
+    }
+
+    p{
+      font-style: italic;
+      color: rgba(241, 241, 241, 0.5);
+      margin: 0;
+      padding-bottom: 50px;
+    }
+  </style>
+</head>
+<body>
+<h1>PHPCalc</h1>
+<p>{my first time using php code}</p>
+<?php
+
+function myCalculator($num01, $oper, $num02) {
+    $sum;
+    // this declaration of sum is necessary to call it
+     switch ($oper){
+      case "add":
+        if ($num01 == null || $num02 == null) {
+          $sum = "You need to enter numbers silly!";
+        } else {
+          $sum = $num01 + $num02;
+        }
+        break;
+      case "sub":
+        if ($num01 == null || $num02 == null) {
+          $sum = "You need to enter numbers silly!";
+        } else {
+          $sum = $num01 - $num02;
+        }
+        break;
+      case "multiply":
+        if ($num01 == null || $num02 == null) {
+          $sum = "You need to enter numbers silly!";
+        } else {
+          $sum = $num01 * $num02;
+        }
+        break;
+      case "divide":
+        if ($num01 == null || $num02 == null) {
+          $sum = "You need to enter numbers silly!";
+        } else {
+          $sum = $num01 / $num02;
+        }
+        break;
+      // good to have default if goes wrong
+      default:
+        $sum = "There's an error!";
+        break;
+     }
+     return $sum;
+}
+
+// creating variables to carry over
+$num01 = $_GET["num01"];
+$oper = $_GET["oper"];
+$num02 = $_GET["num02"];
+
+echo "calculation: " . myCalculator($num01, $oper, $num02);
+echo '<br><br>';
+echo '<a href="/Calculator/project.php">want to do another calculation?</a>';
+?>
+</body>
+</html>
---
+++ project.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>calculator</title>
+  <link rel="icon" href="/Calculator/calc.png" type="image/png">
+  <style>
+    body{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding-top: 100px;
+      background-color: #478589;
+    }
+
+    .body-calc{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding-top: 50px;
+    }
+
+    h1{
+      font-size: 4rem;
+      color: #f1f1f1;
+      margin: 0;
+    }
+
+    p{
+      font-style: italic;
+      color: rgba(241, 241, 241, 0.5);
+      margin: 0;
+    }
+
+    @media (max-width: 400px) {
+      .body-calc{
+        padding-left: 70px;
+      }
+  }
+  </style>
+</head>
+<body>
+  <h1>PHPCalc</h1>
+  <p>{my first time using php code}</p>
+  <div class="body-calc">
+    <form action="functions.php" method="get">
+      <input type="text" name="num01" placeholder="number 1">
+      <select name="oper">
+        <label>Choose Operation!</label>
+        <option value="add">add</option>
+        <option value="sub">minus</option>
+        <option value="multiply">multiply</option>
+        <option value="divide">divide</option>
+      </select>
+      <input type="text" name="num02" placeholder="number 2">
+      <button value="calc" type="submit">calculate</button>
+    </form>
+  </div>
+</body>
+</html>
+
+
+<!-- post is for sensitive data -->
+<!-- select is a dropdown element -->
